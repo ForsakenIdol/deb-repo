c22aa13b80fb7c4e1003f54d08fa844baa67a860
Kali <kali@example.com>
2026-01-18 15:02:28 +0800

    Terraform workflow fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-    name = "apt-repo-resources"
-    location = "Australia East"
-}
+# resource "azurerm_resource_group" "rg" {
+#     name = "apt-repo-resources"
+#     location = "Australia East"
+# }
