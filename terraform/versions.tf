522074d66955043851d8a655e5d9def9cf6ad06e
Kali <kali@example.com>
2026-01-18 14:19:16 +0800

    Formatting updates

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,19 +1,19 @@
 terraform {
-    required_version = ">= 1.14.0"
+  required_version = ">= 1.14.0"
 
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = ">= 4.57.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.57.0"
     }
+  }
 
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "forsakenidol-organization-1"
-        workspaces {
-            name = "deb-repo"
-        }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "forsakenidol-organization-1"
+    workspaces {
+      name = "deb-repo"
     }
+  }
 
 }
