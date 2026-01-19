0235f9a5d7f036d8e5f5671e735ca0707c582609
Kali <kali@example.com>
2026-01-19 16:12:22 +0800

    terraform fmt formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
 resource "azurerm_resource_group" "apt_resources" {
-    name = "apt-repo-resources"
-    location = "Australia East"
+  name     = "apt-repo-resources"
+  location = "Australia East"
 }
 
 resource "azurerm_storage_account" "apt_storage_account" {
-    name = "forsakenidoldebian"
-    resource_group_name = azurerm_resource_group.apt_resources.name
-    location = azurerm_resource_group.apt_resources.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  name                     = "forsakenidoldebian"
+  resource_group_name      = azurerm_resource_group.apt_resources.name
+  location                 = azurerm_resource_group.apt_resources.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "apt_storage_container" {
-    name = "debrepo"
-    storage_account_id = azurerm_storage_account.apt_storage_account.id
-    container_access_type = "container"
+  name                  = "debrepo"
+  storage_account_id    = azurerm_storage_account.apt_storage_account.id
+  container_access_type = "container"
 
 }
 
 output "apt_storage_account_id" {
-    value = azurerm_storage_account.apt_storage_account.id
+  value = azurerm_storage_account.apt_storage_account.id
 }
 
 output "apt_storage_account_endpoint" {
-    value = azurerm_storage_account.apt_storage_account.primary_blob_endpoint
+  value = azurerm_storage_account.apt_storage_account.primary_blob_endpoint
 }
